44f6f7a8663a165524278b55bcbdc845327bdc70
jc23729 <42083637+jc23729@users.noreply.github.com>
2021-03-28 09:55:05 -0400

    update

--- db.js
+++ db.js
@@ -1,2 +1,11 @@
 /** Database setup for BizTime. */
 
+const { Client } = require("pg");
+//Not sure what this means or what line 6 is reffering to postgresql:///biztime
+const client = new Client({
+    connectionString: "postgresql:///biztime"
+});
+
+client.connect();
+
+module.exports = client;
